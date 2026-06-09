cb4185112125e57f31d73713b1847a3048c491ee
stan-hivex <jacintamwenjeri@gmail.com>
2026-06-09 15:40:31 +0300

    admin: add withdrawals admin endpoints and import WithdrawModule

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -3,6 +3,7 @@ import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import { EscrowModule } from '../escrow/escrow.module';
 import { NotificationsModule } from '../notifications/notifications.module';
+import { WithdrawModule } from '../withdraw/withdraw.module';
 
-@Module({ imports: [EscrowModule, NotificationsModule], controllers: [AdminController], providers: [AdminService] })
+@Module({ imports: [EscrowModule, NotificationsModule, WithdrawModule], controllers: [AdminController], providers: [AdminService] })
 export class AdminModule {}
