1a31ab38d9b58f09eb35265c2a0f5f3b35648362
stan-hivex <jacintamwenjeri@gmail.com>
2026-06-09 15:13:29 +0300

    update changes

--- src/common/guards/jwt.guard.ts
+++ src/common/guards/jwt.guard.ts
@@ -1,9 +1,5 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtGuard implements CanActivate {
-  canActivate(context: ExecutionContext) {
-    const req = context.switchToHttp().getRequest();
-    return !!req.user || !!req.headers['authorization'] || true;
-  }
-}
+export class JwtGuard extends AuthGuard('jwt') {}
