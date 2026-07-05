32a690f60110e7bdb241d857e2fce7db83f3beec
stan-hivex <jacintamwenjeri@gmail.com>
2026-07-05 09:14:38 +0300

    Add Prisma migration

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -4,6 +4,7 @@ import { AdminService } from './admin.service';
 import { EscrowModule } from '../escrow/escrow.module';
 import { NotificationsModule } from '../notifications/notifications.module';
 import { WithdrawModule } from '../withdraw/withdraw.module';
+import { AuthModule } from '../auth/auth.module';
 
-@Module({ imports: [EscrowModule, NotificationsModule, WithdrawModule], controllers: [AdminController, SuperadminController], providers: [AdminService] })
+@Module({ imports: [AuthModule, EscrowModule, NotificationsModule, WithdrawModule], controllers: [AdminController, SuperadminController], providers: [AdminService] })
 export class AdminModule {}
