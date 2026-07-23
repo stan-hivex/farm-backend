2598502fff22da0213b5179914a7dff2f23748c5
stan-hivex <jacintamwenjeri@gmail.com>
2026-07-23 14:55:54 +0300

    k

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { AuthModule } from '../auth/auth.module';
 
-@Module({ controllers: [UsersController], providers: [UsersService], exports: [UsersService] })
+@Module({
+  imports: [AuthModule],
+  controllers: [UsersController],
+  providers: [UsersService],
+  exports: [UsersService],
+})
 export class UsersModule {}
