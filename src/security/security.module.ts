7d76c01b5d3ec75935f0753bccff925efc16530e
stan-hivex <jacintamwenjeri@gmail.com>
2026-07-23 14:44:31 +0300

    l

--- src/security/security.module.ts
+++ src/security/security.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { SecurityController } from './security.controller';
 import { SecurityService } from './security.service';
 import { DatabaseModule } from '../database/database.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, AuthModule],
   controllers: [SecurityController],
   providers: [SecurityService],
   exports: [SecurityService],
