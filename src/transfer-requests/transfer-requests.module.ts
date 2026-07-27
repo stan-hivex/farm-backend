d514e844b1f3a85c94a4a02fce0243a77d81310b
stan-hivex <jacintamwenjeri@gmail.com>
2026-07-27 14:14:16 +0300

    k

--- src/transfer-requests/transfer-requests.module.ts
+++ src/transfer-requests/transfer-requests.module.ts
@@ -3,10 +3,11 @@ import { TransferRequestsController } from './transfer-requests.controller';
 import { TransferRequestsService } from './transfer-requests.service';
 import { AuthModule } from '../auth/auth.module';
 import { NotificationsModule } from '../notifications/notifications.module';
+import { SecurityModule } from '../security/security.module';
 import { KycGuard } from '../common/guards/kyc.guard';
 
 @Module({
-  imports: [AuthModule, NotificationsModule],
+  imports: [AuthModule, NotificationsModule, SecurityModule],
   controllers: [TransferRequestsController],
   providers: [TransferRequestsService, KycGuard],
   exports: [TransferRequestsService],
