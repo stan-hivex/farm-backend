0146d022458f423f0e06f8df883422db461db334
stan-hivex <jacintamwenjeri@gmail.com>
2026-08-01 20:24:01 +0300

    k

--- src/kyc/kyc.module.ts
+++ src/kyc/kyc.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { KycController } from './kyc.controller';
 import { KycService } from './kyc.service';
 import { AuthModule } from '../auth/auth.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
-	imports: [AuthModule],
+	imports: [AuthModule, NotificationsModule],
 	controllers: [KycController],
 	providers: [KycService],
 	exports: [KycService],
