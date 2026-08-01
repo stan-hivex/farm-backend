aef18c7be66350e93d512b599dfe56df8d239ed1
stan-hivex <jacintamwenjeri@gmail.com>
2026-08-01 20:40:48 +0300

    l

--- src/transfer-requests/transfer-requests.module.ts
+++ src/transfer-requests/transfer-requests.module.ts
@@ -1,22 +1,15 @@
 import { Module } from '@nestjs/common';
-import { BullModule } from '@nestjs/bull';
 import { TransferRequestsController } from './transfer-requests.controller';
 import { TransferRequestsService } from './transfer-requests.service';
 import { AuthModule } from '../auth/auth.module';
 import { NotificationsModule } from '../notifications/notifications.module';
 import { SecurityModule } from '../security/security.module';
 import { KycGuard } from '../common/guards/kyc.guard';
-import { ExpiryTasksProcessor } from '../common/tasks/expiry-tasks.processor';
 
 @Module({
-  imports: [
-    AuthModule,
-    NotificationsModule,
-    SecurityModule,
-    BullModule.registerQueue({ name: 'expiry-tasks' }),
-  ],
+  imports: [AuthModule, NotificationsModule, SecurityModule],
   controllers: [TransferRequestsController],
-  providers: [TransferRequestsService, KycGuard, ExpiryTasksProcessor],
+  providers: [TransferRequestsService, KycGuard],
   exports: [TransferRequestsService],
 })
 export class TransferRequestsModule {}
